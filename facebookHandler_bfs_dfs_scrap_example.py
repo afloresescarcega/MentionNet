0bd1d4cf3a4134f98a56bd1216ad2fae9ae20c95
killvung <kiulam125@gmail.com>
2016-11-22 01:41:24 -0600

    propse a new way to scrap facebook data

---
+++ facebookHandler_bfs_dfs_scrap_example.py
@@ -0,0 +1,539 @@
+from pprint import pprint as pp
+feed = {
+  "data": [
+    {
+      "from": {
+        "name": "Heran Patel",
+        "id": "10153673408188642"
+      },
+      "id": "155607091223285_1161577443959573",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Ian Smith",
+              "id": "10202516552819221"
+            },
+            "id": "1161881863929131"
+          },
+          {
+            "from": {
+              "name": "Craig Yeh",
+              "id": "3441904295971"
+            },
+            "id": "1162529957197655"
+          },
+          {
+            "from": {
+              "name": "Heran Patel",
+              "id": "10153673408188642"
+            },
+            "id": "1162557783861539"
+          },
+          {
+            "from": {
+              "name": "Craig Yeh",
+              "id": "3441904295971"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Heran Patel",
+                    "id": "10153673408188642"
+                  },
+                  "id": "1162587907191860"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpVNE56a3dOekU1TVRnMk1Eb3hORGM1TnpVNE16a3gZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpVNE56a3dOekU1TVRnMk1Eb3hORGM1TnpVNE16a3gZD"
+                }
+              }
+            },
+            "id": "1162570363860281"
+          },
+          {
+            "from": {
+              "name": "Gabriel Hackworth",
+              "id": "284415605071860"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Heran Patel",
+                    "id": "10153673408188642"
+                  },
+                  "id": "1162816930502291"
+                },
+                {
+                  "from": {
+                    "name": "Heran Patel",
+                    "id": "10153673408188642"
+                  },
+                  "id": "1162817957168855"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpneE5qa3pNRFV3TWpJNU1Ub3hORGM1TnpjeE1qTXgZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpneE56azFOekUyT0RnMU5Ub3hORGM1TnpjeE16ZA3gZD"
+                }
+              }
+            },
+            "id": "1162815037169147"
+          },
+          {
+            "from": {
+              "name": "Ian Smith",
+              "id": "10202516552819221"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Gabriel Hackworth",
+                    "id": "284415605071860"
+                  },
+                  "id": "1162867637163887"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnMk56WXpOekUyTXpnNE56b3hORGM1TnpjMU5ETTAZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnMk56WXpOekUyTXpnNE56b3hORGM1TnpjMU5ETTAZD"
+                }
+              }
+            },
+            "id": "1162834550500529"
+          },
+          {
+            "from": {
+              "name": "Samantha Williamson",
+              "id": "10203404554262061"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Samantha Williamson",
+                    "id": "10203404554262061"
+                  },
+                  "id": "1162870830496901"
+                },
+                {
+                  "from": {
+                    "name": "Craig Yeh",
+                    "id": "3441904295971"
+                  },
+                  "id": "1162873677163283"
+                },
+                {
+                  "from": {
+                    "name": "Samantha Williamson",
+                    "id": "10203404554262061"
+                  },
+                  "id": "1162875333829784"
+                },
+                {
+                  "from": {
+                    "name": "Heran Patel",
+                    "id": "10153673408188642"
+                  },
+                  "id": "1163242270459757"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnM01EZA3pNRFE1Tmprd01Ub3hORGM1TnpjMU56TTIZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTXpJME1qSTNNRFExT1RjMU56b3hORGM1Tnprek5USXkZD"
+                }
+              }
+            },
+            "id": "1162866283830689"
+          },
+          {
+            "from": {
+              "name": "Seymour Buutie",
+              "id": "666872703421061"
+            },
+            "id": "1163279450456039"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRnNE1UZAzJNemt5T1RFek1Ub3hORGM1TnpJeU5EY3kZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTXpJM09UUTFNRFExTmpBek9Ub3hORGM1TnprMk1EVTUZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Ollie Croomes II",
+        "id": "472785456200325"
+      },
+      "id": "155607091223285_1162885683828749",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Luben Popov",
+              "id": "424066887725471"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Ollie Croomes II",
+                    "id": "472785456200325"
+                  },
+                  "id": "1162890543828263"
+                },
+                {
+                  "from": {
+                    "name": "Ollie Croomes II",
+                    "id": "472785456200325"
+                  },
+                  "id": "1162890683828249"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnNU1EVTBNemd5T0RJMk16b3hORGM1TnpjM09UazUZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnNU1EWTRNemd5T0RJME9Ub3hORGM1TnpjNE1ERXoZD"
+                }
+              }
+            },
+            "id": "1162889933828324"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnNE9Ua3pNemd5T0RNeU5Eb3hORGM1TnpjM09URTMZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpnNE9Ua3pNemd5T0RNeU5Eb3hORGM1TnpjM09URTMZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Scott Munro",
+        "id": "10203054790349419"
+      },
+      "id": "155607091223285_1162706537179997",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Chia-Hua Lu",
+              "id": "876566655736794"
+            },
+            "id": "1162721823845135"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpjeU1UZA3lNemcwTlRFek5Ub3hORGM1TnpZAMk1qWTEZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpjeU1UZA3lNemcwTlRFek5Ub3hORGM1TnpZAMk1qWTEZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Craig Yeh",
+        "id": "3441904295971"
+      },
+      "id": "155607091223285_1161888770595107",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Tom",
+              "id": "315757435257647"
+            },
+            "id": "1161900697260581"
+          },
+          {
+            "from": {
+              "name": "Sam Thompson",
+              "id": "10201704946140528"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Cristian Martinez",
+                    "id": "813943805285272"
+                  },
+                  "id": "1162508710533113"
+                },
+                {
+                  "from": {
+                    "name": "William Thomas Waller",
+                    "id": "533837473410328"
+                  },
+                  "id": "1162688343848483"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpVd09EY3hNRFV6TXpFeE16b3hORGM1TnpVek9UVTUZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpZANE9ETTBNemcwT0RRNE16b3hORGM1TnpZAek1EWXcZD"
+                }
+              }
+            },
+            "id": "1162409007209750"
+          },
+          {
+            "from": {
+              "name": "Chia-Hua Lu",
+              "id": "876566655736794"
+            },
+            "id": "1162450033872314"
+          },
+          {
+            "from": {
+              "name": "Alan Ma",
+              "id": "10202751650209165"
+            },
+            "id": "1162506000533384"
+          },
+          {
+            "from": {
+              "name": "Cristian Martinez",
+              "id": "813943805285272"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Chia-Hua Lu",
+                    "id": "876566655736794"
+                  },
+                  "id": "1162591513858166"
+                },
+                {
+                  "from": {
+                    "name": "Craig Yeh",
+                    "id": "3441904295971"
+                  },
+                  "id": "1162604030523581"
+                },
+                {
+                  "from": {
+                    "name": "Cristian Martinez",
+                    "id": "813943805285272"
+                  },
+                  "id": "1162663427184308"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpVNU1UVXhNemcxT0RFMk5qb3hORGM1TnpVNE56azUZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpZAMk16UXlOekU0TkRNd09Eb3hORGM1TnpZAeE1UazAZD"
+                }
+              }
+            },
+            "id": "1162509013866416"
+          },
+          {
+            "from": {
+              "name": "Claire Dubiel",
+              "id": "1029476157080834"
+            },
+            "comments": {
+              "data": [
+                {
+                  "from": {
+                    "name": "Chia-Hua Lu",
+                    "id": "876566655736794"
+                  },
+                  "id": "1162648483852469"
+                }
+              ],
+              "paging": {
+                "cursors": {
+                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpZAME9EUTRNemcxTWpRMk9Ub3hORGM1TnpZAd09ETTUZD",
+                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpZAME9EUTRNemcxTWpRMk9Ub3hORGM1TnpZAd09ETTUZD"
+                }
+              }
+            },
+            "id": "1162627853854532"
+          },
+          {
+            "from": {
+              "name": "Craig Yeh",
+              "id": "3441904295971"
+            },
+            "id": "1162631320520852"
+          },
+          {
+            "from": {
+              "name": "Craig Yeh",
+              "id": "3441904295971"
+            },
+            "id": "1162634210520563"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRrd01EWTVOekkyTURVNE1Ub3hORGM1TnpJeU9ERTMZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTWpZAek5ESXhNRFV5TURVMk16b3hORGM1TnpZAd05UQTIZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Morgan Booth",
+        "id": "1396074624035276"
+      },
+      "id": "155607091223285_1162218127228838"
+    },
+    {
+      "from": {
+        "name": "Diego Maceda",
+        "id": "1134583006579385"
+      },
+      "id": "155607091223285_1161441120639872",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Craig Yeh",
+              "id": "3441904295971"
+            },
+            "id": "1161442583973059"
+          },
+          {
+            "from": {
+              "name": "Diego Maceda",
+              "id": "1134583006579385"
+            },
+            "id": "1161442750639709"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRRME1qVTRNemszTXpBMU9Ub3hORGM1TmprME1qUXoZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRRME1qYzFNRFl6T1Rjd09Ub3hORGM1TmprME1qYzEZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Peter Chan",
+        "id": "172936949783209"
+      },
+      "id": "155607091223285_1161288047321846",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Joshua Pham",
+              "id": "592035814228100"
+            },
+            "id": "1161415720642412"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRReE5UY3lNRFkwTWpReE1qb3hORGM1TmpreE5UTTMZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRReE5UY3lNRFkwTWpReE1qb3hORGM1TmpreE5UTTMZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Raheel Khoja",
+        "id": "10204036710629368"
+      },
+      "id": "155607091223285_1155970861186898",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Peter Chan",
+              "id": "172936949783209"
+            },
+            "id": "1156194311164553"
+          },
+          {
+            "from": {
+              "name": "Raheel Khoja",
+              "id": "10204036710629368"
+            },
+            "id": "1161388187311832"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUxTmpFNU5ETXhNVEUyTkRVMU16b3hORGM1TXpNMU5EYzEZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRNNE9ERTROek14TVRnek1qb3hORGM1TmpnNU56YzUZD"
+          }
+        }
+      }
+    },
+    {
+      "from": {
+        "name": "Ollie Croomes II",
+        "id": "472785456200325"
+      },
+      "id": "155607091223285_1161187323998585"
+    },
+    {
+      "from": {
+        "name": "Gabriel Hackworth",
+        "id": "284415605071860"
+      },
+      "id": "155607091223285_1159849087465742",
+      "comments": {
+        "data": [
+          {
+            "from": {
+              "name": "Kevin Chen",
+              "id": "10152394660823924"
+            },
+            "id": "1161183253998992"
+          }
+        ],
+        "paging": {
+          "cursors": {
+            "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRFNE16STFNems1T0RrNU1qb3hORGM1TmpjeU9ESXcZD",
+            "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEUyTVRFNE16STFNems1T0RrNU1qb3hORGM1TmpjeU9ESXcZD"
+          }
+        }
+      }
+    }
+  ],
+  "paging": {
+    "previous": "https://graph.facebook.com/v2.8/155607091223285/feed?fields=from,comments%7Bfrom,comments%7Bfrom,comments%7Bfrom,comments%7D%7D%7D&limit=10&format=json&icon_size=16&since=1479796058&access_token=EAACEdEose0cBALCwruwSUFMY25wHV1pmqIr27ZCPNEiCeRNQhBsMXSMXSxEeyrWNXwJk6OuQz6uX01zS4zhMZC3LIN5vVT20Ty5lzuUod3iImt8ZCtVZBN7ZCxEpATo9BajTmuOfMeKMpIkvgHDaZAwopdoLoNZAv9B3TmpBltRzVKdXt6a8ZCCn&__paging_token=enc_AdAbk9wbO4XVArFTXZCtKIqEtVX99Abm5rPMjPJYXBTaq28q0IkrLbXfZAfUhTj4a81G9kS8WZAva3onKEOqZAfRzUEbuZBcWfERKtHNFZBLOyVN83eQZDZD&__previous=1",
+    "next": "https://graph.facebook.com/v2.8/155607091223285/feed?fields=from,comments%7Bfrom,comments%7Bfrom,comments%7Bfrom,comments%7D%7D%7D&limit=10&format=json&icon_size=16&access_token=EAACEdEose0cBALCwruwSUFMY25wHV1pmqIr27ZCPNEiCeRNQhBsMXSMXSxEeyrWNXwJk6OuQz6uX01zS4zhMZC3LIN5vVT20Ty5lzuUod3iImt8ZCtVZBN7ZCxEpATo9BajTmuOfMeKMpIkvgHDaZAwopdoLoNZAv9B3TmpBltRzVKdXt6a8ZCCn&until=1479672820&__paging_token=enc_AdCHbpsitJvZBZCNHH389kICuEufcIbRKhZAwQ306zJsD0CZAFx6Q6gXsd6g5ZAmQtHTpTgsSx2voxKhZCdxV7STPUZCfD997s4NfjnZCsjmEaJQHuE53wZDZD"
+  }
+}
+
+def bfs(feed):
+	q = []
+	q.append(feed["data"])		
+	while q:
+		current = q.pop(0)
+		for feed in current:
+			pp(feed["from"]["name"])
+			if "comments" in feed:
+				#Node existed, enqueue it
+				q.append(feed["comments"]["data"])
+
+def dfs_recursive(feed):
+	for feed in feed["data"]:
+		pp(feed["from"]["name"])
+		if "comments" in feed:
+			dfs_recursive(feed["comments"])
+
+# bfs(feed)
+# dfs_recursive(feed)
